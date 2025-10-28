faa5f3f4e481fa49aa2c096a9bb41bf237b03ca8
khalil <khalilbenmeziane@gmail.com>
2025-10-29 00:09:22 +0100

    final

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,11 @@ import { NextResponse, NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token");
-
   if (!token) return NextResponse.redirect(new URL("/", request.url));
-  if (request.nextUrl.pathname === "/") {
+  if (token && request.nextUrl.pathname === "/") {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
+  NextResponse.next();
 }
 
 export const config = {
