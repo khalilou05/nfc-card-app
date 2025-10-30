123bb2dc8b64808de318c540bc5c038e4222e1c6
khalil <khalilbenmeziane@gmail.com>
2025-10-30 15:52:01 +0100

    final

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // images: {
+  //   remotePatterns: [new URL("https://media.twenty-print.com/**")],
+  // },
   images: {
-    remotePatterns: [new URL("https://media.twenty-print.com/**")],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.twenty-print.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
